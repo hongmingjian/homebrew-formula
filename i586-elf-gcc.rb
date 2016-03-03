0afffe46db8657b680f281cab700b801f8d5a679
MingJian Hong <hongmingjian@gmail.com>
2016-03-03 12:22:29 +0800

    Fixed sha checksum

--- i586-elf-gcc.rb
+++ i586-elf-gcc.rb
@@ -4,7 +4,7 @@ class I586ElfGcc < Formula
   homepage "http://gcc.gnu.org"
   url "http://ftpmirror.gnu.org/gcc/gcc-4.9.2/gcc-5.3.0.tar.bz2"
   mirror "ftp://gcc.gnu.org/pub/gcc/releases/gcc-5.3.0/gcc-5.3.0.tar.bz2"
-  sha1 "39ebff8a873181728b02db3689215557628eed9d"
+  sha1 "0612270b103941da08376df4d0ef4e5662a2e9eb"
 
   depends_on 'gmp'
   depends_on 'libmpc'
