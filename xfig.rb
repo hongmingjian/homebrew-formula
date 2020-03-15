3f613776a9376fa0c571208ae74861d9fc6744e4
MingJian Hong <hongmingjian@gmail.com>
2020-03-15 16:05:47 +0800

    Add xfig

---
+++ xfig.rb
@@ -0,0 +1,23 @@
+# Based on https://github.com/mistydemeo/homebrew-xfig
+class Xfig < Formula
+  desc "Interactive drawing tool for X"
+  homepage "https://xfig.org"
+  url "https://downloads.sourceforge.net/project/mcj/xfig-3.2.7b.tar.xz"
+  sha256 "bbc1c4aa8eb7fc06559a5bf83c4798f022be413519778a5d5cdccd24f39a0bce"
+
+  depends_on "fig2dev"
+  depends_on "jpeg"
+  depends_on :x11
+
+  def install
+    system "./configure", "--prefix=#{prefix}",
+                          "--disable-dependency-tracking",
+                          "--without-xfig-libraries",
+                          "--disable-silent-rules"
+    system "make", "install"
+  end
+
+  test do
+    assert_match "Xfig #{version}", shell_output("#{bin}/xfig -v 2>&1")
+  end
+end
