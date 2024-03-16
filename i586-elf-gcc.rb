630c9de49ca5d8e443412a423124d42ca70da0fc
Mingjian Hong <hongmingjian@gmail.com>
2024-03-16 20:27:22 +0800

    gcc 11.2

--- i586-elf-gcc.rb
+++ i586-elf-gcc.rb
@@ -1,9 +1,9 @@
 class I586ElfGcc < Formula
   desc "GNU Compiler Collection targetting i586-elf"
   homepage "https://gcc.gnu.org"
-  url "http://mirrors.ustc.edu.cn/gnu/gcc/gcc-12.2.0/gcc-12.2.0.tar.xz"
+  url "http://mirrors.ustc.edu.cn/gnu/gcc/gcc-11.2.0/gcc-11.2.0.tar.xz"
   version "12.2.0"
-  sha256 "e549cf9cf3594a00e27b6589d4322d70e0720cdd213f39beb4181e06926230ff"
+  sha256 "d08edc536b54c372a1010ff6619dd274c0f1603aa49212ba20f7aa2cda36fa8b"
 
   depends_on "gmp" => :build
   depends_on "i586-elf-binutils"
